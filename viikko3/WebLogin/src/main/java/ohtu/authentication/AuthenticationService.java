97094ee742beb646b84016d143f76a3f939f1143
Elma Nevala <elma.nevala@helsinki.fi>
2020-11-14 14:28:04 +0200

    viikko 3, tehtävä 10

--- viikko3/WebLogin/src/main/java/ohtu/authentication/AuthenticationService.java
+++ viikko3/WebLogin/src/main/java/ohtu/authentication/AuthenticationService.java
@@ -1,5 +1,6 @@
 package ohtu.authentication;
 
+import static java.lang.Character.isLetter;
 import ohtu.data_access.UserDao;
 import ohtu.domain.User;
 import ohtu.util.CreationStatus;
@@ -7,9 +8,11 @@ import ohtu.util.CreationStatus;
 public class AuthenticationService {
 
     private UserDao userDao;
+    private String error;
 
     public AuthenticationService(UserDao userDao) {
         this.userDao = userDao;
+        this.error = "";
     }
 
     public boolean logIn(String username, String password) {
@@ -29,16 +32,48 @@ public class AuthenticationService {
         if (userDao.findByName(username) != null) {
             status.addError("username is already taken");
         }
-
-        if (username.length()<3 ) {
-            status.addError("username should have at least 3 characters");
+        if(invalid(username,password,passwordConfirmation)){
+            System.out.println(this.error);
+            status.addError(this.error);
         }
+
 
         if (status.isOk()) {
             userDao.add(new User(username, password));
         }
         
         return status;
+    }
+    
+    private boolean invalid(String username, String password, String confirmation) {
+        if (username.length()<3 ) {
+            this.error = "username should have at least 3 characters";
+            return true;
+        }
+        if (password.length()<9 ) {
+            this.error = "password should have at least 8 characters";
+            return true;
+        }
+        if (!password.equals(confirmation)) {
+            this.error = "password and password confirmation do not match";
+            return true;
+        }
+        if (hasOnlyLetters(password)) {
+            this.error = "password should have at least one non-letter character";
+            return true;
+        }
+        
+        return false;
+    }
+    
+    private boolean hasOnlyLetters(String password){
+        boolean letter = true;
+        for (int i = 0; i < password.length(); i++){
+            if (!isLetter(password.charAt(i))){
+                letter = false;
+            }
+        }
+        return letter;
     }
 
 }
--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -48,16 +48,50 @@ public class Stepdefs {
     public void nonexsistentUsernameAndPasswordAreGiven(String username, String password) {
         logInWith(username, password);
     }     
+
+    @Given("command new user is selected")
+    public void newUserIsSelected() {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click();   
+    }
     
-//    @When("username {string} and password {string} are given")
-//    public void usernameAndPasswordAreGiven(String username, String password) throws Throwable {
-//        logInWith(username, password);
-//    }   
-//    
-//    @Then("system will respond {string}")
-//    public void systemWillRespond(String pageContent) throws Throwable {
-//        assertTrue(driver.getPageSource().contains(pageContent));
-//    }
+    @When("a valid username {string} and password {string} and matching password confirmation are entered")
+    public void validUsernamePasswordAndConfirmationAreGiven(String username, String password) {
+        signUpWith(username, password, password);
+    }
+    
+    @When("a nonvalid username {string} and password {string} and matching password confirmation are entered")
+    public void nonvalidUsernamePasswordAndConfirmationAreGiven(String username, String password) {
+        signUpWith(username, password, password);
+    }
+    
+    @When("a valid username {string} and a short password {string} and matching password confirmation are entered")
+    public void validUsernameShortPasswordAndConfirmationAreGiven(String username, String password) {
+        signUpWith(username, password, password);
+    }
+    
+    @When("a valid username {string} and a only-letter password {string} and matching password confirmation are entered")
+    public void validUsernameLetterPasswordAndConfirmationAreGiven(String username, String password) {
+        signUpWith(username, password, password);
+    }
+    
+    @When("a valid username {string} and password {string} and wrong confirmation {string} are entered")
+    public void validUsernamePasswordAndWrongConfirmationAreGiven(String username, String password, String confirmation) {
+        signUpWith(username, password, confirmation);
+    }
+    
+    @Then("a new user is created")
+    public void newUserIsCreated() {
+        pageHasContent("Welcome to Ohtu Application!");
+        pageHasContent("continue to application mainpage");
+    }
+    
+    @Then("user is not created and error {string} is reported")
+    public void newUserIsCreated(String error) {
+        pageHasContent("Create username and give password");
+        pageHasContent(error);
+    }
     
     @After
     public void tearDown(){
@@ -79,4 +113,16 @@ public class Stepdefs {
         element = driver.findElement(By.name("login"));
         element.submit();  
     } 
+    
+    private void signUpWith(String username, String password, String confirmation) {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(confirmation);
+        element = driver.findElement(By.name("signup"));
+        element.submit();  
+    }
 }
