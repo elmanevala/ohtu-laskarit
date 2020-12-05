cc873ff8a9e686455de7113b6ee675e4c2eaaba2
Elma Nevala <elma.nevala@helsinki.fi>
2020-12-05 13:55:46 +0200

    viikko 6, tehtävä 3

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -8,15 +8,16 @@ public class Main {
         Statistics stats = new Statistics(new PlayerReaderImpl("https://nhlstatisticsforohtu.herokuapp.com/players.txt"));
 
         Matcher m = new And(
-                new HasFewerThan(1, "goals"),
-                new PlaysIn("NYR")
+                new HasAtLeast(50, "points"),
+                new Or(
+                        new PlaysIn("NYR"),
+                        new PlaysIn("NYI"),
+                        new PlaysIn("BOS")
+                )
         );
-
         for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
-        
-        System.out.println(stats.matches(new All()).size());
 
     }
 }
